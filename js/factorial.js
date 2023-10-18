efe28622de1d8c41342d9c816f704e636f67c9ec
Jazmin <jazminillera0@gmail.com>
2023-10-18 18:39:57 -0500

    ejercicios realizado

--- js/factorial.js
+++ js/factorial.js
@@ -1,16 +1,30 @@
-function convertirTemperatura() {
-  let temperaturaCelsius = parseFloat(document.getElementById("celsius").value);
-  let resultadoDiv = document.getElementById("resultado");
+function calculateFactorial() {
+  let input = prompt("Por favor, ingresa un número para calcular su factorial:");
+  let num = parseFloat(input);
 
-  if (isNaN(temperaturaCelsius)) {
-    resultadoDiv.innerHTML = "<p>Por favor, ingrese un número válido.</p>";
-    return;
+  if (!isNaN(num)) {
+    if (num < 0) {
+      showError("No se puede calcular el factorial de un número negativo.");
+    } else if (Number.isInteger(num)) {
+      let factorial = 1;
+      for (let i = 1; i <= num; i++) {
+        factorial *= i;
+      }
+      showResult(`El factorial de ${num} es ${factorial}.`);
+    } else {
+      showError("El número ingresado no es un número entero.");
+    }
+  } else {
+    showError("Entrada inválida. Por favor, ingresa un número válido.");
   }
+}
 
-  const temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
-  const temperaturaKelvin = temperaturaCelsius + 273.15;
+function showResult(message) {
+  const resultContainer = document.getElementById("result");
+  resultContainer.textContent = message;
+}
 
-  resultadoDiv.innerHTML = `
-    <p>Grados Kelvin: ${temperaturaKelvin.toFixed(2)}</p>
-    <p>Grados Fahrenheit: ${temperaturaFahrenheit.toFixed(2)}</p>`;
+function showError(message) {
+  const errorContainer = document.getElementById("error");
+  errorContainer.textContent = message;
 }
