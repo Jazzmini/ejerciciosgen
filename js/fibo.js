1c2bf20eb0a62cbcdf7c120a824abc347b9e36c9
Jazmin <jazminillera0@gmail.com>
2023-10-25 18:13:46 -0500

    Se realiza ejercicio de fibonacci

--- js/fibo.js
+++ js/fibo.js
@@ -1,30 +1,21 @@
-function calculateFactorial() {
-  let input = prompt("Por favor, ingresa un número para calcular su factorial:");
-  let num = parseFloat(input);
+function calcularFibonacci() {
+  const inputNumber = document.getElementById("inputNumber").value;
+  const output = document.getElementById("output");
 
-  if (!isNaN(num)) {
-    if (num < 0) {
-      showError("No se puede calcular el factorial de un número negativo.");
-    } else if (Number.isInteger(num)) {
-      let factorial = 1;
-      for (let i = 1; i <= num; i++) {
-        factorial *= i;
-      }
-      showResult(`El factorial de ${num} es ${factorial}.`);
-    } else {
-      showError("El número ingresado no es un número entero.");
-    }
-  } else {
-    showError("Entrada inválida. Por favor, ingresa un número válido.");
+  // Verificar si la entrada es un número válido
+  const numero = parseInt(inputNumber);
+  if (isNaN( numero)|| numero < 1) {
+      output.textContent = "Por favor, ingresa un número válido.";
+      return;
   }
-}
 
-function showResult(message) {
-  const resultContainer = document.getElementById("result");
-  resultContainer.textContent = message;
-}
+  // Calcular la serie de Fibonacci
+  const fibonacciArray = [0, 1];
+  for (let i = 2; i < numero; i++) {
+      const nextNumber = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+      fibonacciArray.push(nextNumber);
+  }
 
-function showError(message) {
-  const errorContainer = document.getElementById("error");
-  errorContainer.textContent = message;
+  // Mostrar la serie de Fibonacci en el DOM
+  output.textContent = fibonacciArray.join(", ");
 }
